0eceb0fac0c80c7952a43d165b3d40e70b46fbe2
“Gerien66” <jemchankm@gmail.com>
2022-06-29 20:48:47 +0800

    tried to change redirectui

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,5 +1,6 @@
 const clientId = "2a5802209d6d498b87bf801754ee8ad5";
-const redirectUri = "https://gerien66.github.io/React-Jamming-Project/";
+const redirectUri =
+    "https://gerien66.github.io/React-Jamming-Project/callback/";
 let accessToken;
 
 const Spotify = {
