ed0f6ccce4d6231fdc68b57aadd6105d879f9f23
“Gerien66” <jemchankm@gmail.com>
2022-06-29 18:53:26 +0800

    first commit

---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./TrackList.css";
+import { Track } from "../Track/Track";
+
+export class TrackList extends React.Component {
+    render() {
+        const renderedItems = this.props.tracks.map((track) => {
+            return (
+                <Track
+                    track={track}
+                    key={track.id}
+                    onAdd={this.props.onAdd}
+                    onRemove={this.props.onRemove}
+                    isRemoval={this.props.isRemoval}
+                />
+            );
+        });
+
+        return <div className="TrackList">{renderedItems}</div>;
+    }
+}
